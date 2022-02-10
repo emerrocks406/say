ccd02ea8d9b9023ac72113eb568d0658d4f68b27
Emer Rocks <emer.rocks@unosquare.com>
2022-02-10 14:39:16 +0000

    say project

---
+++ say.go
@@ -0,0 +1,12 @@
+// Package sort provides pritimitives for sorting slices and user-defined
+// collections.
+package say
+
+import "fmt"
+
+// Hello returns a string saying hello to the name of
+// the person that has been provided.
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello %s.", name)
+}
